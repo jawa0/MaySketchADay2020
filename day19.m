b6b7cf94df88daf39b02c014dd873f244ecc7d6d
Jaba Adams <jaba.adams@gmail.com>
2020-05-20 01:06:47 -0400

    Restore overwritted day19.m

--- day19.m
+++ day19.m
@@ -1,27 +1,38 @@
-%% #MaySketchADay - Day 01
+% #MaySketchADay Day 19
+% No attempt at good code quality. The idea of MaySketchADay is to
+% combat perfectionism. #generativeart #proceduralart is already one step
+% removed from sketching, and was getting me into an overly deliberative
+% headspace. So, I'm just going to drop some fugly code and try to make a
+% pretty picture. Jabavu Adams.
 
-WIDTH_PIXELS = 1280;
-HEIGHT_PIXELS = 720;
+W = 1920;
+H = 1080;
+white = [1 1 1];
 
-a_img = zeros(HEIGHT_PIXELS, WIDTH_PIXELS, 3);
-a_img(100, 100, 1) = 1.0;
-a_img(200, 200, 2) = 1.0;
-a_img(300, 300, 3) = 1.0;
-a_img(400, 400, :) = [1.0 1.0 1.0];
+img = zeros(H, W, 3);
 
-draw_circle(a_img, [500 500], 100, [1 1 1]);
+for i = 1:int32(1000 + 100*randn())
+	r = int32(3 + randn());
+	x = W*rand();
+	y = H*randn();
+	img = fillCircle(img, x, y, r, [100*rand()/255.0 (190 + 20*randn())/255.0 (255 +  20*randn())/255.0]);
+end
 
-image(a_img)
+imwrite(img, "2020-05-19.png");
 
-%% Draw a Circle
+function img = fillCircle(im, cx, cy, r, rgb)
+	rSquared = r*r;
+	for y = cy-r:cy+r
+		for x = cx-r:cx+r
+			if x < 1 || x > 1920 || y < 0 || y > 1080
+				continue
+			end
 
-function draw_circle(img, cx, cy, r, rgb, a)
-    msaa = 4;
-    
-    top = cy - r;
-    bottom = y + r;
-    left = cz - r;
-    right = cx + r;
-    
-    px_left = 
+			dSquared = (x - cx)^2 + (y - cy)^2;
+			if dSquared <= rSquared
+				im(int16(y + 0.5), int16(x + 0.5), :) = rgb;
+			end
+		end
+	end
+	img = im;
 end
